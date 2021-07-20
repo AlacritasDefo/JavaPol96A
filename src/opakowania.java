233344908c9f49716202b20d5318eb9dde7bcdaf
darecc <dariusz.ceglarek@gmail.com>
2021-07-20 22:05:45 +0200

    Dodanie programu sprawdzajacego czy liczba jest doskonala

--- src/opakowania.java
+++ src/opakowania.java
@@ -1,13 +1,7 @@
 import java.util.*;
 
-//Wyświetlenie podzielników (całkowitych) liczby X
-//Suma podzielników liczby X
-//Czy liczba jest doskonała (suma podzielników równa liczbie, np. 6 = 1 + 2 + 3)
-//Szukamy liczby powyzej x, ktora jest doskonala
-
 public class opakowania {
     public static void main(String[] args) {
-        HashMap<Integer, List<Integer>> mapa = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
         int L1 = 0;
         int L2 = 0;
@@ -27,29 +21,6 @@ public class opakowania {
         System.out.println("Podaj ile tabletek jest aktualnie w opakowaniu trzecim: ");
         int opakowanieL3=scanner.nextInt();
         do {
-            if (mapa.containsKey(opakowanieL1)) {
-                List<Integer> lista = mapa.get(opakowanieL1);
-                lista.add(opakowanieL2);
-                mapa.put(opakowanieL1, lista);
-            }
-            else {
-                List<Integer> lista = new ArrayList<>();
-                lista.add(opakowanieL2);
-                mapa.put(opakowanieL1, lista);
-            }
-            if (dniSuma > 5000) {
-                System.out.println("WYNIKI");
-                for(var entry : mapa.entrySet ()) {
-                    System.out.print(entry.getKey() + " : ");
-                    int ile = entry.getValue().size();
-                    if (ile > 30)
-                        ile = 30;
-                    for(int i = 0; i < ile; i++)
-                        System.out.print(entry.getValue().get(i) + ", ");
-                    System.out.println();
-                }
-                return;
-            }
             if (opakowanieL1==0 && opakowanieL2 == 0 && opakowanieL3 == 0) {
                 koniec=true;
                 break;
@@ -58,6 +29,8 @@ public class opakowania {
             if (opakowanieL2 == 0 ) opakowanieL2 = L2;
             if (opakowanieL3 == 0 ) opakowanieL3 = L3;
             dniSuma ++;
+            if (dniSuma > 100_000)
+                break;
             opakowanieL1--;
             System.out.print(" L1: "+opakowanieL1);
             opakowanieL2--;
@@ -66,6 +39,9 @@ public class opakowania {
             System.out.print(" L3: "+opakowanieL3);
             System.out.println();
         } while (!koniec);
-        System.out.println("Wszystkie opakowania jednocześnie skończą się po: "+ dniSuma+" dni");
+        if (koniec == true)
+            System.out.println("Wszystkie opakowania jednocześnie skończą się po: "+ dniSuma+" dni");
+        else
+            System.out.println("Chyba nigdy nie nastąpi jednoczesne skończenie się tabletek!");
     }
 }
